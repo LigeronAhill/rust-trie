d8f06bdd08b4cd0516afdb93fcbbfb038c7b5b94
Aleksandr <provotorov@inbox.ru>
2024-05-13 21:20:09 +0300

    len

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+
+#[derive(Debug, Default)]
+pub struct Node {
+    pub at_end: bool,
+    pub children: HashMap<u8, Node>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Default)]
+pub struct Trie {
+    pub root: Node,
+    pub len: usize,
+}
+impl Trie {
+    pub fn new() -> Self {
+        Trie::default()
+    }
+    pub fn insert(&mut self, text: &str) {
+        let mut current_node = &mut self.root;
+        for c in text.bytes() {
+            current_node = current_node.children.entry(c).or_default();
+        }
+        current_node.at_end = true;
+        self.len += 1;
+    }
+    pub fn contains(&self, text: &str) -> bool {
+        let mut current_node = &self.root;
+        for c in text.bytes() {
+            match current_node.children.get(&c) {
+                Some(node) => current_node = node,
+                None => return false,
+            }
+        }
+        current_node.at_end
+    }
+    pub fn len(&self) -> usize {
+        self.len
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use rust_trie::Trie;
 
 fn main() {
     let mut trie = Trie::new();
@@ -12,41 +12,4 @@ fn main() {
     let not_contains_2 = trie.contains("https://www");
     dbg!(not_contains_2);
     dbg!(trie.len());
-}
-
-#[derive(Debug, Default)]
-struct Node {
-    at_end: bool,
-    children: HashMap<u8, Node>,
-}
-
-#[derive(Debug, Default)]
-struct Trie {
-    root: Node,
-    len: usize,
-}
-impl Trie {
-    fn new() -> Self {
-        Trie::default()
-    }
-    fn insert(&mut self, text: &str) {
-        let mut current_node = &mut self.root;
-        for c in text.bytes() {
-            current_node = current_node.children.entry(c).or_default();
-        }
-        current_node.at_end = true;
-    }
-    fn contains(&self, text: &str) -> bool {
-        let mut current_node = &self.root;
-        for c in text.bytes() {
-            match current_node.children.get(&c) {
-                Some(node) => current_node = node,
-                None => return false,
-            }
-        }
-        current_node.at_end
-    }
-    fn len(&self) -> usize {
-        self.len
-    }
 }
